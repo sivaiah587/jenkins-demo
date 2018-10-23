3d964d6c1fbac7c5c2d66c31dc008949b2e88b7a
User <User@DESKTOP-NL91EAB>
2018-10-23 14:48:09 +0530

    second commit

--- src/main/java/com/jenkin/controller/HelloController.java
+++ src/main/java/com/jenkin/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.jenkin.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class HelloController {
-	
+	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String Welcome()
 	{
 		return "Wecome Jenkins Demo!";
